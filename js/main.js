0db4119f32a4e4b0897fcdf2fb2cc7f5e4e5dfa2
summ-it-us <sccsbyaff@gmail.com>
2020-07-23 22:41:06 +0900

    add a nav animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("hello");
+// Function expression to select elements `
+
+const selectElement = (s) => document.querySelector(s);
+const navLinks = document.querySelectorAll(".nav-link");
+
+selectElement(".burger-icon").addEventListener("click", () => {
+    selectElement(".nav-list").classList.toggle("active");
+    selectElement(".burger-icon").classList.toggle("toggle")
+
+    navLinks.forEach((link,index) => {
+        if (link.style.animation){
+            link.style.animation = ""
+        }else{
+            link.style.animation = `navLinkAnimate 0.5s ease forwards ${ index/7 + 0.5}s`
+            console.log(index/7 + 0.5)
+        }
+    })
+});
